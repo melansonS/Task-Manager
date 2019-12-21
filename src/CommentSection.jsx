2075b40932977c665f07a0775ef2172f1f884691
melansonS <samuelmelanson@live.com>
2019-12-21 10:25:41 -0500

    fixed comment section styling typo

--- src/CommentSection.jsx
+++ src/CommentSection.jsx
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
-import "./styling/commentSection.css";
+import "./styling/CommentSection.css";
 
 class UnconnectedCommentSection extends Component {
   constructor(props) {
